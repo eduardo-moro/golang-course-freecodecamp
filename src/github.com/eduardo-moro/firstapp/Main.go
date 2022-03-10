116716a0fff57cab12e763739cca1d6570263aa0
eduardo-moro <eduardomoro1127@gmail.com>
2022-03-10 16:52:14 -0300

    Testing Truth table with bit operations

--- src/github.com/eduardo-moro/firstapp/Main.go
+++ src/github.com/eduardo-moro/firstapp/Main.go
@@ -1,5 +1,35 @@
 package main
 
+import "fmt"
+
 func main() {
+	/*
+	 * &  = AND
+	 * |  = OR
+	 * ^  = XOR
+	 * &^ = NOR
+	 */
+
+	a := 10 //1010
+	b := 3  //0011
+
+	fmt.Println(a & b)  // 0010 -> 2
+	fmt.Println(a | b)  // 1011 -> 11
+	fmt.Println(a ^ b)  // 1001 -> 9
+	fmt.Println(a &^ b) // 0100 -> 8
+
+	/*
+	 * The following code does not work because this operands are only for 'bit' operations
+	 */
+
+	/*
+		var c bool = true
+		var d bool = false
+
+		fmt.Println(c & d)
+		fmt.Println(c | d)
+		fmt.Println(c ^ d)
+		fmt.Println(c &^ d)
+	*/
 
 }
