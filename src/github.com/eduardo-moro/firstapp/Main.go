24c1242a397085bee69d26c31e7a31f96c0d906d
eduardo-moro <eduardomoro1127@gmail.com>
2022-03-10 17:06:34 -0300

    Testing bit shifting

--- src/github.com/eduardo-moro/firstapp/Main.go
+++ src/github.com/eduardo-moro/firstapp/Main.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	a := 8 //2^3
+
+	fmt.Println(a << 3) // 2^(3 * 3) = 2^6 // --> 64
+	fmt.Println(a >> 3) // 2^(3 / 3) = 2^0 // --> 1
+
+	/*
+	 * i = 8
+	 * j = 2
+	 *
+	 * i << j == i^j
+	 * or (2^3)^3 = 2^(3 * 3)
+	 */
 
 }
